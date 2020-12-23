83ab7532dcd36a3dc3710cac0a73f069e66f54fe
Laura Heino <lhhein@gmail.com>
2020-12-23 14:06:21 +0100

    quiz game logic and more topics added

--- src/script.js
+++ src/script.js
@@ -7,59 +7,111 @@ const topicSelector = document.querySelector('#topics')
 
 let correctAnswer;
 let score = 0
+let round = 0
+let mode;
 
 const baseUrl = 'https://opentdb.com/api.php?amount=10&category='
 const urlGeneral = '9&type=multiple'
 const urlMovies = '11&type=multiple'
 const urlScience = '17&type=multiple'
 const urlAnimals = '27&type=multiple'
-const urlGeography= '22&type=multiple'
-const urlComputers= '18&type=multiple'
+const urlGeography = '22&type=multiple'
+const urlComputers = '18&type=multiple'
+const urlVideogames = '15&type=multiple'
+const urlTelevision = '14&type=multiple'
 
 
-
-btn.addEventListener('click', () => {
+topicSelector.addEventListener('click', () => {
+    clearResponse()
     getData()
 })
 
+document.querySelector('#send').addEventListener('click', () => {
+    checkResponse()
+
+})
+document.querySelector('#next').addEventListener('click', () => {
+    document.querySelector('#next').classList.add('inactive')
+    document.querySelector('#send').classList.remove('inactive')
+    clearResponse()
+    getData()
+})
 
 function getQuestions() {
-    if (topicSelector.value === 'general') 
-        return `${baseUrl}${urlGeneral}`
-    
-    else if (topicSelector.value === 'movies') 
-        return `${baseUrl}${urlMovies}`
-    
-    else if (topicSelector.value === 'science') 
-        return `${baseUrl}${urlScience}`
-            
-    else if (topicSelector.value === 'animals') 
-    return `${baseUrl}${urlAnimals}`
-        
-    else if (topicSelector.value === 'geography') 
-        return `${baseUrl}${urlGeography}`
-            
-    else if (topicSelector.value === 'computers') 
-    return `${baseUrl}${urlComputers}`
+
+    if (round === 0) {
+        if (topicSelector.value === 'general') {
+            mode = 'general'
+        }
+        else if (topicSelector.value === 'movies') {
+            mode = 'movies'
+        }
+        else if (topicSelector.value === 'science')  {
+            mode = 'science'
+        }         
+        else if (topicSelector.value === 'animals') {
+            mode = 'animals'
+        }  
+        else if (topicSelector.value === 'geography') {
+            mode = 'geography' 
+        }    
+        else if (topicSelector.value === 'computers') {
+            mode = 'computers'
+        }
+        else if (topicSelector.value === 'videogames') {
+            mode = 'videogames'
+        }
+        else if (topicSelector.value === 'television') {
+            mode = 'television'
+        }
+    }
+
+        if (mode === 'general') {
+            return `${baseUrl}${urlGeneral}`
+        }
+        else if (mode === 'movies') {
+            return `${baseUrl}${urlMovies}`
+        }
+        else if (mode === 'science') {
+            return `${baseUrl}${urlScience}`
+        }
+        else if (mode === 'animals') {
+            return `${baseUrl}${urlAnimals}`
+            }
+        else if (mode === 'geography') {
+            return `${baseUrl}${urlGeography}`
+        }
+        else if (mode === 'computers') {
+            return `${baseUrl}${urlComputers}`
+        }
+        else if (mode === 'videogames') {
+            return `${baseUrl}${urlVideogames}`
+        }
+        else if (mode === 'television') {
+            return `${baseUrl}${urlTelevision}`
+        }
+
+
 }
 
 
 async function getData() {
     document.querySelector('#question-div').classList.remove('hidden')
-    const response = await fetch(getQuestions())
 
+    const  response = await fetch(getQuestions())
+  
     try {
         const data = await response.json()
        
         let dataObj = {
-            question: data.results[0].question,
+            question: data.results[round].question,
             options:  [
-                data.results[0].correct_answer,
-                data.results[0].incorrect_answers[0],
-                data.results[0].incorrect_answers[1],
-                data.results[0].incorrect_answers[2],
+                data.results[round].correct_answer,
+                data.results[round].incorrect_answers[0],
+                data.results[round].incorrect_answers[1],
+                data.results[round].incorrect_answers[2],
             ],
-            difficulty: data.results[0].difficulty
+            difficulty: data.results[round].difficulty
         }
 
         q.innerHTML = dataObj.question
@@ -72,6 +124,9 @@ async function getData() {
         }
         
         correctAnswer = dataObj.options[0]
+        document.querySelector('#q-number').innerHTML = `Question ${round+1}`
+        document.querySelector('#score').innerHTML =  `Score: ${score} / 10`
+
 
     }
     catch (err) {
@@ -80,25 +135,39 @@ async function getData() {
 }
 
 
-document.querySelector('#send').addEventListener('click', () => {
-    checkResponse()
-
-})
 
 
 function checkResponse() {
 
+    for (let i = 0; i < responseArray.length; i++) {
+        if (responseArray[i].value === correctAnswer && responseArray[i].checked) {
+            optionsArray[i].classList.add('correct')
+            score++
+        }
+        else if  (responseArray[i].value !== correctAnswer && responseArray[i].checked) {
+            optionsArray[i].classList.add('incorrect')
+        }
 
-for (let i = 0; i < responseArray.length; i++) {
-    if (responseArray[i].value === correctAnswer && responseArray[i].checked) {
-        optionsArray[i].classList.add('correct')
-        score++
     }
-    else if  (responseArray[i].value !== correctAnswer && responseArray[i].checked) {
-        optionsArray[i].classList.add('incorrect')
+    round++
+    document.querySelector('#next').classList.remove('inactive')
+    document.querySelector('#send').classList.add('inactive')
+    if(round === 10) {
+        document.querySelector('#next').classList.add('inactive')
+        document.querySelector('#send').classList.add('inactive')
+        document.querySelector('#score').classList.add('highlighted')
     }
+
 }
 
+function clearResponse() {
+    responseArray.forEach(item => {
+        item.checked = false
+    })
+
+    optionsArray.forEach(item => {
+        item.classList.remove('correct', 'incorrect')
+    })
 
 }
 
