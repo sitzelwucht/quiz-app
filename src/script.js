32ca43824422e6370452d50f6380af72f2388297
Laura Heino <lhhein@gmail.com>
2020-12-23 09:49:27 +0100

    some logic added

--- src/script.js
+++ src/script.js
@@ -1,29 +1,46 @@
 
 const q = document.querySelector('#question')
 const optionsArray = document.querySelectorAll('.options')
+const responseArray = document.querySelectorAll('.responses')
 const btn = document.querySelector('#startBtn')
+const topicSelector = document.querySelector('#topics')
+
+let correctAnswer;
+let score = 0
+
+const baseUrl = 'https://opentdb.com/api.php?amount=10&category='
+const urlGeneral = '9&type=multiple'
+const urlMovies = '11&type=multiple'
+const urlScience = '17&type=multiple'
+const urlAnimals = '27&type=multiple'
+const urlGeography= '22&type=multiple'
+const urlComputers= '18&type=multiple'
 
 
-
-const urlGeneral = 'https://opentdb.com/api.php?amount=10&category=9&type=multiple'
-const urlMovies = 'https://opentdb.com/api.php?amount=10&category=11&type=multiple'
-const urlScience = 'https://opentdb.com/api.php?amount=10&category=17&type=multiple'
 
 btn.addEventListener('click', () => {
     getData()
 })
 
 
-
 function getQuestions() {
-    if (document.querySelector('#topics').value === 'general') 
-        return urlGeneral
+    if (topicSelector.value === 'general') 
+        return `${baseUrl}${urlGeneral}`
     
-    else if (document.querySelector('#topics').value === 'movies') 
-        return urlMovies
+    else if (topicSelector.value === 'movies') 
+        return `${baseUrl}${urlMovies}`
     
-    else if (document.querySelector('#topics').value === 'science') 
-    return urlScience
+    else if (topicSelector.value === 'science') 
+        return `${baseUrl}${urlScience}`
+            
+    else if (topicSelector.value === 'animals') 
+    return `${baseUrl}${urlAnimals}`
+        
+    else if (topicSelector.value === 'geography') 
+        return `${baseUrl}${urlGeography}`
+            
+    else if (topicSelector.value === 'computers') 
+    return `${baseUrl}${urlComputers}`
 }
 
 
@@ -48,9 +65,13 @@ async function getData() {
         q.innerHTML = dataObj.question
 
         let optionsData = [ dataObj.options[0], dataObj.options[1], dataObj.options[2], dataObj.options[3] ]
-        for (let i = 0; i < optionsData.length; i++) {
-            optionsArray[i].innerHTML = optionsData[i]
+        let shuffledOptions = shuffleArray(optionsData)
+        for (let i = 0; i < shuffledOptions.length; i++) {
+            optionsArray[i].innerHTML = shuffledOptions[i]
+            responseArray[i].value = shuffledOptions[i]
         }
+        
+        correctAnswer = dataObj.options[0]
 
     }
     catch (err) {
@@ -59,6 +80,29 @@ async function getData() {
 }
 
 
+document.querySelector('#send').addEventListener('click', () => {
+    checkResponse()
+
+})
+
+
+function checkResponse() {
+
+
+for (let i = 0; i < responseArray.length; i++) {
+    if (responseArray[i].value === correctAnswer && responseArray[i].checked) {
+        optionsArray[i].classList.add('correct')
+        score++
+    }
+    else if  (responseArray[i].value !== correctAnswer && responseArray[i].checked) {
+        optionsArray[i].classList.add('incorrect')
+    }
+}
+
+
+}
+
 function shuffleArray(arr) {
     arr = arr.sort(() => Math.random() - 0.5)
+    return arr
 }
